a598ff4adc5daa40648ca388e331699839a44534
Michael OConnor <gmikeoc@gmail.com>
2018-03-25 17:07:25 -0500

    Significant rewrite, added additional error handling

--- URL_monitor.py
+++ URL_monitor.py
@@ -11,7 +11,7 @@
 + either bail out or print error with a timestamp.
 +
 + Program also uses AWS SNS service to send text message to phone number in
-+ form of +12345679999
++ form of +12345679999 and at this time only works for US country code '+1'
 +
 + Tested on:
 +
@@ -23,8 +23,10 @@
 __author__      = "Michael E. O'Connor"
 __copyright__   = "Copyright 2018"
 
+import sys
 import time
 import boto3
+import signal
 import requests
 from os import environ
 from hashlib import sha1
@@ -45,11 +47,11 @@ target_file = '/tmp/pymonitor.html'
 # Note this is used by both the requests() call as well as the latency calculation
 # which is dependent on client host performance and workload
 
-target_timeout = 1.5
+target_timeout = 2.0
 
-# How often to test, or sleep between tests, in seconds
+# How often to test, or sleep between tests (in seconds)
 
-test_interval = 300
+test_interval = 5
 
 # Return current date and time with local TZ
 
@@ -60,6 +62,9 @@ def t_stamp():
     time_msg = time.strftime('\n%Y-%m-%d %H:%M:%S %Z: ', time.localtime(t))
 
     return (time_msg)
+
+# Routine to send SMS message utilizing AWS Simple Notification service
+# Assumes CELL_PHONE enviroment variable is set correctly
 
 def send_sms(client, sms_msg):
 
@@ -80,6 +85,9 @@ def validate_environment():
     try:
         aws_profile = environ["AWS_PROFILE"]
         resp = input ("AWS_PROFILE set to [{0}]. Press enter to confirm or specify new: ".format(aws_profile))
+
+        # If response if not NULL, change profile value to new
+
         if bool(resp.strip()):
             print ("Profile changed to [{0}]".format(resp))
             aws_profile = resp
@@ -89,37 +97,52 @@ def validate_environment():
 
     environ["AWS_PROFILE"] = aws_profile
 
-    try:
-        target_phone = environ["CELL_PHONE"]
-        resp = input ("CELL_PHONE set to [{0}]. Press enter to confirm or specify new: ".format(target_phone))
-        if bool(resp.strip()):
-            print ("Mobile changed to [{0}]".format(resp))
-            target_phone = resp
+    while True:
 
-    except KeyError:
-        target_phone = input ("CELL_PHONE not set. Please enter a valid Mobile # [+12345679999]: ")
+        try:
+            target_phone = environ["CELL_PHONE"]
+            resp = input ("CELL_PHONE set to [{0}]. Press enter to confirm or specify new: ".format(target_phone))
 
-    environ["CELL_PHONE"] = target_phone
+            # If response if not NULL, change phone number to new number provided
+
+            if bool(resp.strip()):
+                print ("Mobile changed to [{0}]".format(resp))
+                target_phone = resp
+
+        except KeyError:
+            target_phone = input ("CELL_PHONE not set. Please enter a valid Mobile # [+12345679999]: ")
+
+        # Validate phone number format, break if correct, warn and loop if not
+
+        if (len(target_phone) == 12) and (target_phone[0:2] == '+1'):
+            environ["CELL_PHONE"] = target_phone
+            break
+        else:
+            print ("Error: phone # must start with +1 followed by 10 digits")
 
     return
 
 #----------------------------------------------------------------------------
-# Confirm user is known to AWS and that use has required SNS authorization
+# Confirm user is known to AWS and that user has required SNS authorization
+# Assumes that environment variables have been previously set
 # ---------------------------------------------------------------------------
 
 def check_authorization(service):
 
+    mobile = environ["CELL_PHONE"]
+    user = environ["AWS_PROFILE"]
+
     try:
         client = boto3.client(service)
-        client.publish(PhoneNumber=environ["CELL_PHONE"], Message="Begin Monitoring")
+        client.publish(PhoneNumber=mobile, Message="Begin Monitoring")
 
     except ProfileNotFound:
-        print ("Error: No AWS credentials found for [" + environ["AWS_PROFILE"] + "]")
-        print ("To configure, run: aws configure --profile " + environ["AWS_PROFILE"])
+        print ("Error: AWS credentials not found for [{}]".format(user))
+        print ("To configure, run: aws configure --profile " + user)
         return False
 
     except ClientError:
-        print ("Error: AWS User doesn't have adequate permissions")
+        print ("Error: User [{}] has inadequate AWS permissions".format(user))
         return False
 
     return client
@@ -134,8 +157,8 @@ def main():
 
     validate_environment()
 
-    # Confim that we're able to establish an AWS client sessions and have
-    # adequate permission to send SMS messages
+    # Confim that we're able to establish an AWS client session and have
+    # adequate permission to send SMS messages via AWS SNS
 
     client = check_authorization('sns')
 
@@ -144,17 +167,23 @@ def main():
     if client == False:
         quit()
 
-    # Initialize for good measure
+    # Initialize variables for good measure. All will be reset in main loop
 
-    err_msg = "No Trouble Found"
+    err_msg = "**This should never happen**"
 
-    # SHA1 hash of Target URL. Initially set to bogus value as will be reset on 1st pass
+    target_Hash = "not yet set"
 
-    target_Hash = '010101010110101010101010101'
-
-    print ("\nMonitoring web site: {}".format(target_URL))
+    first_Pass = True
 
     while True:
+
+        # Sleep for specified time interval, if not first time through loop
+
+        if first_Pass == True:
+            print ("\nMonitoring web site: {}".format(target_URL))
+            first_Pass = False
+        else:
+            time.sleep(test_interval)
 
         # Send get request to specified URL and test for errors
 
@@ -164,66 +193,80 @@ def main():
             resp_URL = requests.get(target_URL, timeout=target_timeout)
             latency = timer() - start
 
+            # Check for problems with HTTP response and parse errors
+
             resp_URL.raise_for_status()
 
-        # For HTTP or Timeout errors, alert, sleep for awhile and loop back to top
+        # For HTTP, Timeout or Connection errors, alert and loop back to top
 
         except requests.exceptions.HTTPError as e1:
             err_msg = "HTTP Error: " + str(e1)
             print(t_stamp() + err_msg)
             send_sms(client, target_URL + '\n' + err_msg)
-            time.sleep(test_interval)
             continue
+
         except requests.exceptions.Timeout as e2:
             err_msg = "Timeout Error: " + str(e2)
             print(t_stamp() + err_msg)
-            send_sms(client, target_URL + '\n' + err_msg)
-            time.sleep(test_interval)
+            send_sms(client, target_URL + '\n' + "Timeout error")
             continue
-
-        # For Connection or Unknown errors, exit program immediately
 
         except requests.exceptions.ConnectionError as e3:
             err_msg = "Connection Error: " + str(e3)
             print(t_stamp() + err_msg)
-            send_sms(client, target_URL + "Abort Monitoring")
-            quit()
+            send_sms(client, target_URL + '\n' + "Connection error")
+            continue
+
+        # For Unknown errors, exit program immediately
+
         except requests.exceptions.RequestException as e4:
             err_msg = "Unknown Error: " + str(e4)
             print(t_stamp() + err_msg)
-            send_sms(client, target_URL + "Abort Monitoring")
-            quit()
+            send_sms(client, target_URL + '\n' + "Abort Monitoring")
+            break
 
         # Calculate latency on URL get request. Complain if too long
 
         if latency >= target_timeout:
-            err_msg = "Latency is: {0:4.2f}, threshold: {1}".format(latency, target_timeout)
+            err_msg = " Response took: {0:4.2f}s, threshold: {1}s".format(latency, target_timeout)
             print(t_stamp() + err_msg)
-            send_sms(client, target_URL + '\n' + err_msg)
+            send_sms(client, target_URL + '\n' + "Slow response")
 
         # Compute SHA1 hash of the URL contents so we can compare against previous.
-        # If changed, report then reset target to current
+        # If changed, report and then reset target hash to current hash value
 
         current_Hash = sha1(resp_URL.content).hexdigest()
 
         if current_Hash != target_Hash:
-            err_msg = "Hash reset to: {}".format(current_Hash)
+            err_msg = " Hash reset to: {}".format(current_Hash)
             print(t_stamp() + err_msg)
-            send_sms(client, target_URL + '\n' + err_msg)
+            send_sms(client, target_URL + '\n' + "Web site content changed")
             target_Hash = current_Hash
 
-        # Save URL contents to a file in case we want to examine further
+        # Save URL contents to local filesystem in case we want to examine further
 
-##        file = open(target_file, "w+")
-##        file.write(resp_URL.text)
-##        file.close()
+        file = open(target_file, "w+")
+        file.write(resp_URL.text)
+        file.close()
 
         print('.', end='', flush=True)    # Output stream of dots to represent progress
 
-        time.sleep(test_interval)
+    # If reaching this point, something has gone wrong
+
+    print(t_stamp() + "Monitoring Abnormally Terminated")
+
+    sys.exit(1)
+
+# Signal handler for CTRL-C manual termination
+
+def signal_handler(signal, frame):
+    print("\nProgram exiting gracefully")
+    sys.exit(0)
 
 # If called from shell as script
 
 if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, signal_handler)
 
     main()
